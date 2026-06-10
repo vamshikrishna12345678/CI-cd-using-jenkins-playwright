92160584a6d353158a186f3159eb39e6eb19ca27
vamshikrishna12345678 <chagantivamshikrishna1995@gmail.com>
2026-06-10 10:07:55 +0530

    added testdata

---
+++ testdata/data.js
@@ -0,0 +1,22 @@
+export const loginTestData = [
+    {
+        email: "laura.taylor1234@example.com",
+        password: "test123",
+        validity: "valid"
+    },
+    {
+        email: "invaliduser1@test.com",
+        password: "sdadasdasd",
+        validity: "invalid"
+    },
+    {
+        email: "invaliduser2@test.com",
+        password: "sadasdasdsd",
+        validity: "invalid"
+    },
+    {
+        email: "",
+        password: "",
+        validity: "invalid"
+    }           
+]
